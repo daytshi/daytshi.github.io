4ff41f4f56dc1f937b2c0230eec725cc6a657004
daytshi <46690144+daytshi@users.noreply.github.com>
2022-06-07 23:05:46 +0200

    Update swapp.php

--- swapp.php
+++ swapp.php
@@ -1,6 +1,9 @@
 <?php
-$user=$_POST['user'];
-echo $user;
-$txt=fopen('file.txt','a') or die ('lopalopa');
-fwrite($txt,$user."\n");
+// $user=$_POST['user'];
+// echo $user;
+// $txt=fopen('file.txt','a') or die ('lopalopa');
+// fwrite($txt,$user."\n");
+?>
+<?php
+echo 'hola todos';
 ?>
